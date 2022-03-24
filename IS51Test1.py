2e57176ec425b2dd6f6b231f60108bcb3c74a02f
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-03-24 15:59:01 -0700

    committed structured english

---
+++ IS51Test1.py
@@ -0,0 +1,13 @@
+"""Structured-english
+start
+We need to decide which form of payment is higher, Option1 or Option2
+Option1 is 100 dollars a day for ten days
+Option2 is 1 dollar the first day then doubles each day (1,2,4,8, etc)
+First we need to calulate the total pay for each option
+Option1 = 100 * 10
+Option2 = (1+2+4+8, etc) till list indexs reachs 9
+If option1 == option2, then print "Option1 and Option2 pays the same"
+Then, if option one is higher print "Option1 is better" to the console
+Else print "Option2 is better"
+end
+"""
